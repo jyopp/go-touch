d252d1121271b30e332395c72481ba479fdaf2ce
James Yopp <git@jyopp.com>
2021-08-09 09:21:13 -0700

    ImageLayer

---
+++ image.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+)
+
+type ImageLayer struct {
+	BasicLayer
+	Image      image.Image
+	Background color.Color
+	Centered   bool
+}
+
+func (i *ImageLayer) Init(frame Rect, image image.Image) *ImageLayer {
+	i.Image = image
+	i.Centered = true
+	i.BasicLayer.Init(frame, i)
+	i.Background = color.Transparent
+	return i
+}
+
+func (i *ImageLayer) Draw(layer Layer, ctx DrawingContext) {
+	if _, _, _, a := i.Background.RGBA(); a > 0x10 {
+		model565.Fill(ctx, i.Bounds(), i.Background)
+	}
+
+	if i.Image != nil {
+		var negativeOffset image.Point
+		if i.Centered {
+			negativeOffset.X = (i.Image.Bounds().Dx() - i.w) / 2
+			negativeOffset.Y = (i.Image.Bounds().Dy() - i.h) / 2
+		}
+		draw.Draw(ctx, ctx.Bounds(), i.Image, negativeOffset, draw.Over)
+	}
+}
